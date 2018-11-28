a15c66625d67c1e6c9ee7af926748117b082780a
nelma <nelmasb@gmail.com>
2018-11-28 21:04:22 -0200

    primeiro commit

---
+++ ex-commonJS/index.js
@@ -0,0 +1,4 @@
+console.log('Olá');
+
+const logger = require('./logger');
+logger.info('logger - usando o padrão commonJS')
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+
+//visivel fora do arquivo
+module.exports = {
+  entry: './ex/index.js',
+  output: {
+    path: __dirname + '/public',
+    filename: './bundle.js'
+  },
+  devServer: {
+    port: 8080,
+    contentBase: './public'
+  },
+  module: {
+    //é quem vai carregar os arquivos
+    loaders: [{
+      test: /.js?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  }
+}
